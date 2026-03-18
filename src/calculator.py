0b0f61e97b936c8eff3ac6d2f7da3b57fda3aa4d
soufiane051 <soufiane.bouchaane@enim.ac.ma>
2026-03-18 14:20:37 +0000

    Update calculator.py

--- src/calculator.py
+++ src/calculator.py
@@ -9,3 +9,10 @@ def add(a, b):
 def subtract(a, b):
     """Soustrait deux nombres."""
     return a - b
+    
+def divide(a, b):
+    """Retourne la division de a par b. Lance une exception"""
+    """ si elle detecte une erreur."""
+    if b == 0:
+        raise ValueError("Division par zero est impossible")
+    return a / b    
