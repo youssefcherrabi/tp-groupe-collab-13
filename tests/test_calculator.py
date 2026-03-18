cab90dd94a07662b8da7d500cb655fa22c37ee80
soufiane051 <soufiane.bouchaane@enim.ac.ma>
2026-03-18 14:21:41 +0000

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,3 +7,15 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 2) == 3
+
+import pytest
+from src.calculator import divide
+
+def test_divide():
+    assert divide(10, 2) == 5.0
+    assert divide(7, 2) == 3.5
+
+# Cas limite
+def test_divide_by_zero():
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(5, 0)
